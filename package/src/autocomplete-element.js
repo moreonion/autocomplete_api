e486b5c532b8e023347d09ceb8c57f6247c98c74
Roman Zimmermann <roman@more-onion.com>
2019-09-03 14:13:35 +0200

    Refactor autocomplete elment into its own class.

---
+++ package/src/autocomplete-element.js
@@ -0,0 +1,47 @@
+/* global Drupal, jQuery */
+
+var $ = jQuery
+
+class AutocompleteElement {
+  constructor ($element, settings) {
+    this.$element = $element
+    this.settings = settings
+  }
+  select2Config () {
+    return $.extend(true, {
+      minimumInputLength: 2,
+      ajax: {
+        url: Drupal.settings.autocomplete_api.endpoint,
+        data: this.buildData,
+        processResults: this.processResults.bind(this),
+        dataType: 'json',
+        delay: 250,
+        beforeSend: function (xhr) {
+          xhr.setRequestHeader('Authorization', Drupal.settings.autocomplete_api.apiKey)
+        }
+      }
+    }, this.settings, {
+      dropdownParent: this.$element.parent()
+    })
+  }
+  bindSelect2 () {
+    this.$element.select2(this.select2Config())
+  }
+  buildData (params) {
+    return {
+      search: params.term,
+      count: 20,
+      offset: ((params.page || 1) - 1) * 20
+    }
+  }
+  processResults (data, page) {
+    return {
+      results: data.values.map(function (o) {
+        return { id: JSON.stringify(o), text: o.label }
+      }),
+      more: page * 20 < data.total_items
+    }
+  }
+}
+
+export { AutocompleteElement }
--- package/src/main.js
+++ package/src/main.js
@@ -1,54 +1,18 @@
 /* global Drupal, jQuery */
 
+import { AutocompleteElement } from './autocomplete-element'
+
 var $ = jQuery
+
 Drupal.behaviors.autocomplete_api = {
-  buildData: function (params) {
-    return {
-      search: params.term,
-      count: 20,
-      offset: ((params.page || 1) - 1) * 20
-    }
-  },
-  processResults: function (data, page) {
-    return {
-      results: data.values.map(function (o) {
-        return {
-          id: JSON.stringify(o),
-          text: o.label
-        }
-      }),
-      more: page * 20 < data.total_items
-    }
-  },
-  defaultConfig: function () {
-    return {
-      minimumInputLength: 2,
-      ajax: {
-        url: Drupal.settings.autocomplete_api.endpoint,
-        data: this.buildData,
-        processResults: this.processResults,
-        dataType: 'json',
-        delay: 250,
-        beforeSend: function (xhr) {
-          xhr.setRequestHeader('Authorization', Drupal.settings.autocomplete_api.apiKey)
-        }
-      }
-    }
-  },
   attach: function (context, settings) {
-    var $element, config, elementConfig, htmlId, parentConfig, ref
-    ref = settings.autocomplete_api.elements
-    for (htmlId in ref) {
-      elementConfig = ref[htmlId]
-      $element = $(`#${htmlId}`, context)
-      parentConfig = {
-        dropdownParent: $element.parent()
-      }
-      config = $.extend(true, {}, this.defaultConfig(), elementConfig, parentConfig)
-      $element.select2(config)
+    let configs = settings.autocomplete_api.elements
+    for (let htmlId in configs) {
+      (new AutocompleteElement($(`#${htmlId}`, context), configs[htmlId])).bindSelect2()
     }
   }
 }
+
 Drupal.webform = Drupal.webform || {}
 Drupal.webform.conditionalOperatorAutocompleteEqual = function (element, existingValue, ruleValue) {
   var data, e
